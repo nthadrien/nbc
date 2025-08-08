ab4fda1cf28ef76cc83bf3a4fbaea124d1f28302
nthadrien <hadrien.wakacode@gmail.com>
2025-08-08 02:22:04 +0100

    translation almost done

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,14 +19,34 @@ export const languages = {
       'about.story1':"We found that many ambitious projects lacked the necessary support, whether at the planning, financing or evaluation level. This is why we have combined our skills to create a complete and integrated solution.",
       'about.vision':'We have helped many organizations realize their goals, providing them with the tools and expertise they need to thrive.',
       'about.vision.catch':"Our services are designed for a wide range of organizations.",
-      'about.biz.one':'NGO or association',
+      'about.biz.one':'NGO or Association',
       'about.biz.one.decs':'You are an NGO or association seeking to finance a social or humanitarian project and need help with fundraising and monitoring & evaluation.',
       'about.biz.two':'Startup or Growing company',
       'about.biz.two.decs':'You are a growing company preparing an expansion project and needs a feasibility study and rigorous project management.',
-      'about.biz.three':'individual entrepreneur',
+      'about.biz.three':'Individual (Entrepreneur)',
       'about.biz.three.decs':'You are an individual entrepreneur with an innovative idea who needs a strategic plan to attract investors and monitoring to ensure the success of your project.',
       'about.biz.four':'Public institution',
       'about.biz.four.decs':"You are a public institution launching a program and looking for a partner for monitoring and evaluating its impact.",
+      
+      'faq.def':"Frequently Asked Questions",
+      'faq.question-one':"Qu'est-ce que le fundraising ?",
+      'faq.answer-one':"Le fundraising, ou levée de fonds, est le processus de sollicitation de dons, de subventions ou d'investissements auprès de particuliers, d'entreprises, de fondations ou d'institutions pour financer un projet ou une organisation.",
+      'faq.question-two':"Comment menez-vous une étude de projet ?",
+      'faq.answer-two':"Nous réalisons une analyse approfondie qui inclut l'étude de faisabilité, l'évaluation des risques, la planification stratégique et la définition des objectifs pour assurer que votre projet est viable et bien structuré avant son lancement.",
+      'faq.question-three':"Quelle est l'importance du suivi et de l'évaluation (S&E) ?",
+      'faq.answer-three':"Le S&E est crucial pour mesurer la performance et l'impact de votre projet. Il permet de s'assurer que les objectifs sont atteints, d'identifier les points d'amélioration et de prendre des décisions éclairées pour l'avenir.",
+      'faq.question-four':"Est-ce que vos services s'appliquent à tous les secteurs d'activité ?",
+      'faq.answer-four':"Oui, nos services sont flexibles et peuvent être adaptés à divers secteurs, qu'il s'agisse de l'éducation, de la santé, de la technologie, de l'environnement ou du développement communautaire.",
+      
+      'team.def':"Our Team",
+      'team.meet':"Meet the Team",
+      "team.desc":"We are a team of dynamic and experienced professionals, united by a passion and commitment to provide quality, personalized and tailor-made solutions.",
+
+      "test.client":"Customer Testimonials",
+      "test.desc":"Here's what our customers say about us",
+
+      "slogan":"Transform our clients' visions into tangible successes."
+
     },
     fr: {
       'nav.home': 'Accueil',
@@ -34,19 +54,40 @@ export const languages = {
       'nav.contact':'Contactez Nous',
       'nav.news':"Actualités",
       'nav.who':'Qui nous sommes?',
-      "read":"Lire",
+      "read":"Lire plus",
       // pages
       'about.desc':'Nous livrons et fournissons toujours des solutions efficaces pour vos besoins de gestion financière',
       'about.story':"Nti Business consulting a été fondée par des experts passionnés, notre cabinet est né avec une mission claire : transformer les visions de nos clients en réussites tangibles.",
       'about.story1':"Nous avons constaté que de nombreux projets ambitieux manquaient du soutien nécessaire, que ce soit au niveau de la planification, du financement ou de l'évaluation. C'est pourquoi nous avons uni nos compétences pour créer une solution complète et intégrée.",
       'about.vision':"Au fils des annees nous avons aidé de nombreuses organisations à concrétiser leurs objectifs, en leur fournissant les outils et l'expertise dont elles ont besoin pour prospérer.",
       'about.vision.catch':"Nos services sont conçus pour une large gamme d'organisations.",
-      'about.biz.one.decs':'',
-      'about.biz.two':'',
-      'about.biz.two.decs':'',
-      'about.biz.three':'',
-      'about.biz.three.decs':'',
-      'about.biz.four':'',
-      'about.biz.four.decs':'',
+      'about.biz.one':'ONG ou Association',
+      'about.biz.one.decs':"Vous êtes une ONG ou une association cherchant à financer un projet social ou humanitaire et avez besoin d'aide pour la collecte de fonds et le suivi-évaluation.",
+      'about.biz.two' : 'Startup ou Entreprise en croissance',
+      'about.biz.two.decs':"Vous êtes une entreprise en pleine croissance préparant un projet d'expansion et avez besoin d'une étude de faisabilité et d'une gestion de projet rigoureuse.",
+      'about.biz.three' : "Entrepreneur individuel",
+      'about.biz.three.decs':"Vous êtes un entrepreneur individuel avec une idée innovante qui a besoin d'un plan stratégique pour attirer les investisseurs et d'un suivi pour assurer le succès de votre projet.",
+      'about.biz.four' : "Institution Publique",
+      'about.biz.four.decs':"Vous êtes une institution publique lançant un programme et recherchez un partenaire pour suivre et évaluer son impact.",
+      
+      'faq.def':'Foire aux Questions',
+      'faq.question-one':"Qu'est-ce que le fundraising ?",
+      'faq.answer-one':"Le fundraising, ou levée de fonds, est le processus de sollicitation de dons, de subventions ou d'investissements auprès de particuliers, d'entreprises, de fondations ou d'institutions pour financer un projet ou une organisation.",
+      'faq.question-two':"Comment menez-vous une étude de projet ?",
+      'faq.answer-two':"Nous réalisons une analyse approfondie qui inclut l'étude de faisabilité, l'évaluation des risques, la planification stratégique et la définition des objectifs pour assurer que votre projet est viable et bien structuré avant son lancement.",
+      'faq.question-three':"Quelle est l'importance du suivi et de l'évaluation (S&E) ?",
+      'faq.answer-three':"Le S&E est crucial pour mesurer la performance et l'impact de votre projet. Il permet de s'assurer que les objectifs sont atteints, d'identifier les points d'amélioration et de prendre des décisions éclairées pour l'avenir.",
+      'faq.question-four':"Est-ce que vos services s'appliquent à tous les secteurs d'activité ?",
+      'faq.answer-four':"Oui, nos services sont flexibles et peuvent être adaptés à divers secteurs, qu'il s'agisse de l'éducation, de la santé, de la technologie, de l'environnement ou du développement communautaire.",
+
+      'team.def':"Notre Équipe",
+      'team.meet':"Rencontrez l'Équipe",
+      "team.desc":"Nous sommes une équipe de professionnels dynamiques et expérimentés, unis par une passion et un engagement de fournir des accompagnement de qualité, personnalisé et des solutions sur mesure.",
+
+      "test.client":"Témoignages de Clients",
+      "test.desc":"Voici ce que nos clients disent de nous",
+
+      "slogan":"Transformer les visions de nos clients en succès tangibles."
+
     },
   } as const;
